d7783bff46d86a7dc378233d7c1dca03613c2d9e
Ivan Verhun <902581+iverhun@users.noreply.github.com>
2021-03-14 01:48:50 +0200

    step 5 - using Google AutoService to generate META-INF/services/...

--- lib/src/main/java/ua/lviv/javaclub/annotation/processor/CoffeeJugMagicBuilderProcessor.java
+++ lib/src/main/java/ua/lviv/javaclub/annotation/processor/CoffeeJugMagicBuilderProcessor.java
@@ -1,11 +1,14 @@
 package ua.lviv.javaclub.annotation.processor;
 
+import com.google.auto.service.AutoService;
 import java.util.Set;
 import javax.annotation.processing.AbstractProcessor;
+import javax.annotation.processing.Processor;
 import javax.annotation.processing.RoundEnvironment;
 import javax.lang.model.SourceVersion;
 import javax.lang.model.element.TypeElement;
 
+@AutoService(Processor.class)
 public class CoffeeJugMagicBuilderProcessor extends AbstractProcessor {
 
   @Override

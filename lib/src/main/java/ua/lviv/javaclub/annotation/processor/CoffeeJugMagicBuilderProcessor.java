48f7ae7c7bee933a345235a046db45b884e0af0a
Ivan Verhun <902581+iverhun@users.noreply.github.com>
2021-03-15 00:45:11 +0200

    step 6 - added templates for generated code

--- lib/src/main/java/ua/lviv/javaclub/annotation/processor/CoffeeJugMagicBuilderProcessor.java
+++ lib/src/main/java/ua/lviv/javaclub/annotation/processor/CoffeeJugMagicBuilderProcessor.java
@@ -17,6 +17,35 @@ public class CoffeeJugMagicBuilderProcessor extends AbstractProcessor {
     return false;
   }
 
+/*
+    var builderClassCodeTemplate = """
+        package %1$s;
+
+        public class %2$s {
+          %4$s
+
+          private %2$s() {}
+
+          public static %2$s builder() {
+            return new %2$s();
+          }
+
+          public %3$s build() {
+            return new %3$s(%5$s);
+          }
+        }
+        """;
+
+    var fieldAndMethodCode = """
+          private %2$s %3$s;
+
+          public %1$s %3$s(%2$s %3$s) {
+            this.%3$s = %3$s;
+            return this;
+          }
+        """;
+*/
+
   @Override
   public Set<String> getSupportedAnnotationTypes() {
     return Set.of("ua.lviv.javaclub.annotation.processor.CoffeeJugMagic");
